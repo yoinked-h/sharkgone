bb50384a76df7e66c15c910e9469237dad969151
evazion <noizave@gmail.com>
2025-05-06 03:50:37 -0500

    Fix #5912: Recognize Patreon creator URLs
    Fix `https://www.patreon.com/c/yaisirdrawz` URLs being mangled into `https://www.patreon.com/c`

--- app/logical/source/url/patreon.rb
+++ app/logical/source/url/patreon.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Source::URL::Patreon < Source::URL
-  RESERVED_USERNAMES = %w[api bePatron card-teaser-image collection checkout file home join
+  RESERVED_USERNAMES = %w[api bePatron c card-teaser-image collection checkout file home join
                           login m messages notifications policy posts profile search settings user]
 
   attr_reader :username, :user_id, :post_id, :attachment_id, :title, :media_hash, :media_params
@@ -46,6 +46,7 @@ class Source::URL::Patreon < Source::URL
     # https://www.patreon.com/bePatron?u=4045578
     # https://www.patreon.com/user?u=5993691
     # https://www.patreon.com/user/posts?u=84592583
+    # https://www.patreon.com/profile/creators?u=32306860
     in _, "patreon.com", *rest if params[:u].present?
       @user_id = params[:u]
 
