40d33b2b61ef426d25e645dbee32ee9957d9ccac
evazion <noizave@gmail.com>
2025-05-28 06:17:51 -0500

    controllers: lower rate limits.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   respond_to :html, :xml, :json
 
-  rate_limit :create, rate: 1.0/5.minutes, burst: 10
+  rate_limit :create, rate: 1.0/5.minutes, burst: 5
 
   def new
     @user = authorize User.new
