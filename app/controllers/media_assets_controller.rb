cbea8471b204674a8ed719c87403ed60872deaa8
evazion <noizave@gmail.com>
2025-06-01 01:48:08 -0500

    Fix #5975: Don't Flash Messages on the Non-HTML Versions of Endpoints.
    Add `notice` argument to `respond_with` that sets the flash appropriately based on the type of response.

--- app/controllers/media_assets_controller.rb
+++ app/controllers/media_assets_controller.rb
@@ -40,8 +40,8 @@ class MediaAssetsController < ApplicationController
   def destroy
     @media_asset = authorize MediaAsset.find(params[:id])
     @media_asset.trash!(CurrentUser.user)
-    flash[:notice] = "File deleted"
-    respond_with(@media_asset)
+
+    respond_with(@media_asset, notice: "File deleted")
   end
 
   def image
