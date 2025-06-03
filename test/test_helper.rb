7849b2156defad53ccf912e63b3ab0a9cb9afe6f
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2025-06-03 08:25:13 +0000

    Refactor external URL parsing tests
    This commit splits parsing and extracting tests into different files for ease of testing and readability.
    It also provides multiple new methods to automate the testing of parsing methods.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 
-require 'simplecov'
+require "simplecov"
 require_relative "../config/environment"
-require 'rails/test_help'
+require "rails/test_help"
 
 Dir["#{Rails.root}/test/factories/*.rb"].sort.each { |file| require file }
 Dir["#{Rails.root}/test/test_helpers/*.rb"].sort.each { |file| require file }
@@ -10,7 +10,7 @@ Dir["#{Rails.root}/test/test_helpers/*.rb"].sort.each { |file| require file }
 Minitest::Reporters.use!([
   Minitest::Reporters::ProgressReporter.new,
   Minitest::Reporters::HtmlReporter.new(reports_dir: "tmp/html-test-results"),
-  Minitest::Reporters::JUnitReporter.new("tmp/junit-test-results")
+  Minitest::Reporters::JUnitReporter.new("tmp/junit-test-results"),
 ])
 
 Shoulda::Matchers.configure do |config|
@@ -30,7 +30,8 @@ class ActiveSupport::TestCase
   include DatabaseTestHelper
   include IqdbTestHelper
   include UploadTestHelper
-  include SourceTestHelper
+  include ExtractorTestHelper
+  include UrlTestHelper
   extend NormalizeAttributeHelper
 
   unless Danbooru.config.debug_mode
@@ -115,3 +116,5 @@ class ActionDispatch::IntegrationTest
     method_authenticated(:delete, url, user, **options)
   end
 end
+
+module Source::Tests; end
