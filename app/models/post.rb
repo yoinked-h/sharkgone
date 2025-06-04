e8185963d71e50a01d7bf86416d5a13ab06f676a
evazion <noizave@gmail.com>
2025-06-04 00:36:28 -0500

    Fix #6004: Notes can cover media controls on mobile.
    Fix it so that it's not possible to add notes to ugoira or video posts. Adding notes to videos and
    ugoiras was disallowed before, but due to a regression it became possible to add notes to ugoiras.
    
    Also fix the "Add note" link appearing and doing nothing on posts where it isn't possible to add
    notes, and fix it being possible to add notes to video posts through the API.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1017,6 +1017,10 @@ class Post < ApplicationRecord
   end
 
   concerning :NoteMethods do
+    def can_have_notes?
+      is_image?
+    end
+
     def has_notes?
       last_noted_at.present?
     end
