536bc13bf419c0579fb15d36d52b90b0e3f8a799
evazion <noizave@gmail.com>
2025-07-06 17:26:23 -0500

    news updates: use dtext instead of html for messages.
    * Use DText instead of HTML for displaying messages.
    * Limit max message length.
    * Normalize and strip spaces from messages.

--- app/models/news_update.rb
+++ app/models/news_update.rb
@@ -7,11 +7,14 @@ class NewsUpdate < ApplicationRecord
   scope :active, -> { recent.where(is_deleted: false) }
 
   deletable
+  dtext_attribute :message, inline: true
+
   scope :active, -> { undeleted.where("created_at + duration >= ?", Time.zone.now) }
 
+  normalizes :message, with: ->(message) { message.to_s.normalize_whitespace.unicode_normalize(:nfc).strip }
   validate :validate_duration, if: :duration_changed?
   validate :validate_active, on: :create
-  validates :message, presence: true, if: :message_changed?
+  validates :message, presence: true, length: { maximum: 280 }, if: :message_changed?
 
   def self.visible(user)
     if user.is_admin?
