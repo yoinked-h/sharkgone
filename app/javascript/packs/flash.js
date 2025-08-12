6c82cf310f6b6e27d222e3ce35a67d7960c4a0d9
Talulah <talulah@reunion.is>
2025-08-12 19:06:19 -0300

    Update NPM packages.

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,5 +1,5 @@
 import "@ruffle-rs/ruffle/ruffle.js";
-import "@ruffle-rs/ruffle/core.ruffle.d7b4e9136951d7ba899e.js";
-import "@ruffle-rs/ruffle/core.ruffle.58241492c8289478c844.js";
-import "@ruffle-rs/ruffle/4316626e20ec3b22d6c1.wasm";
-import "@ruffle-rs/ruffle/9287ce844015bfaf85c8.wasm";
+import "@ruffle-rs/ruffle/core.ruffle.bc1c9e34dd90fe6e31b5.js";
+import "@ruffle-rs/ruffle/core.ruffle.b6b32bd51abfe7cbf897.js";
+import "@ruffle-rs/ruffle/4471ac35b3412ea3a4f0.wasm";
+import "@ruffle-rs/ruffle/c762d25423edcbf93ed6.wasm";
