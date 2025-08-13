7e37e3e4f2475febefc054335713fe8ae7d83c58
Talulah <talulah@reunion.is>
2025-08-12 21:06:36 -0300

    Update structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -2212,7 +2212,8 @@ CREATE TABLE public.user_events (
     user_agent character varying,
     metadata jsonb,
     fingerprint jsonb,
-    fingerprint_hash text
+    fingerprint_hash text,
+    login_session_id uuid
 );
 
 
@@ -3331,7 +3332,6 @@ ALTER TABLE ONLY public.ip_bans
 
 ALTER TABLE ONLY public.ip_geolocations
     ADD CONSTRAINT ip_geolocations_pkey PRIMARY KEY (id);
-
 
 
 --
@@ -5965,6 +5965,13 @@ END)));
 
 
 --
+-- Name: index_user_events_on_login_session_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_user_events_on_login_session_id ON public.user_events USING btree (login_session_id);
+
+
+--
 -- Name: index_user_events_on_metadata; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -6707,6 +6714,14 @@ ALTER TABLE ONLY public.bulk_update_requests
 
 
 --
+-- Name: user_events fk_rails_89475bdf6f; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.user_events
+    ADD CONSTRAINT fk_rails_89475bdf6f FOREIGN KEY (login_session_id) REFERENCES public.login_sessions(login_id);
+
+
+--
 -- Name: login_sessions fk_rails_8c949dd2cd; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -6969,6 +6984,8 @@ ALTER TABLE ONLY public.upload_media_assets
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20250720155738'),
+('20250718142035'),
 ('20250716202530'),
 ('20250716150524'),
 ('20250603085358'),
